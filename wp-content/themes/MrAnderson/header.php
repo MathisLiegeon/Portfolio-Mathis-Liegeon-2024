e087e5cad6b3f211629fab94cb0cbb953e34e544
Mathis <150056959+MathisLiegeon@users.noreply.github.com>
2024-10-18 15:52:23 +0200

    menu html

--- wp-content/themes/MrAnderson/header.php
+++ wp-content/themes/MrAnderson/header.php
@@ -30,20 +30,42 @@
             ));
           ?>
 
+          <div class="menu-contact">
+            <div class="menu-contact-text">
+              <h2 class="menu-contact-title">Contactez moi</h2>
+              <p class="menu-contact-email">liegeon.mathis@gmail.com</p>
+            </div>
+
+            <div class="menu-contact-social-container">
+              <a href="" class="menu-contact-social">In</a>
+              <a href="" class="menu-contact-social">Insta</a>
+              <a href="" class="menu-contact-social">Bé</a>
+            </div>
+          </div>
+
           <div class="footer">
-          <p>© 2023 Weedwey Corporation. All rights reserved.</p>
+            <div class="footer-container">
+              <h3 class="footer-title">Compétences</h3>
+                <p class="footer-skills">Design Graphique</p>
+                <p class="footer-skills">Illustrations</p>
+                <p class="footer-skills">Front-end</p>
+                <p class="footer-skills">Back-end</p>
+              <span class="footer-content">
+
+              </span>
+            </div>
+
+            <div class="footer-container">
+              <h3 class="footer-title"></h3>
+                <a href="" class="footer-links">Instagram⇗</a>
+                <a href="" class="footer-links">LinkedIn⇗</a>
+                <a href="" class="footer-links">Behance⇗</a>
+                <a href="" class="footer-links">Email⇗</a>
+              <span class="footer-content">
+
+              </span>
+            </div>
           </div>
         </nav>
 
   </header>
-
-      <!-- <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var burger = document.getElementById('burger');
-            var mobileMenu = document.getElementById('mobile-menu');
-
-            burger.addEventListener('click', function() {
-                mobileMenu.classList.toggle('open');
-            });
-        });
-    </script> -->
